364408d91074303099cb5378545d5aa54b2fea3c
gihong-park <dev.gihong2012@gmail.com>
2023-04-10 16:34:12 +0900

    fix: some pages are complete

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
+  },
+  webpack: config => {
+    // 아래를 추가합니다.
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
   }
 }
 
---
+++ src/hoc/SectionWrapper.tsx
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+import { Component, ReactNode } from "react";
+
+import { styles } from "src/styles/styles";
+import { staggerContainer } from "src/utils/motion";
+import { JsxElement } from "typescript";
+
+export default function StartWrapper(component: () => JSX.Element, idName: string) {
+  return function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer(0, 0)}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: true, amount: 0.25 }}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+        <span className='hash-span' id={idName}>
+          &nbsp;
+        </span>
+
+      {component()}
+      </motion.section>
+    );
+  };
+}
