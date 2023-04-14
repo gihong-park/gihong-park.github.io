80d8cca3572478cc5cd7b160116d805864c4e3ad
gihong-park <dev.gihong2012@gmail.com>
2023-04-14 18:17:39 +0900

    fix: head text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -34,8 +34,8 @@ const About = () => {
   return (
     <>
       <motion.div variants={textVariant(0)}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <p className={"sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider"}>Introduction</p>
+        <h2 className={"text-white font-black text-[30px] md:text-[60px] sm:text-[50px] xs:text-[40px]"}>Overview.</h2>
       </motion.div>
 
       <motion.p
